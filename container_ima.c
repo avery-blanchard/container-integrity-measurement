b8e78ae6f29e24509476caa0a09cdc5bd24c388a
Avery Blanchard <avery-blanchard@proton.me>
2023-10-23 18:30:58 -0400

    Handle memory errors in FS traversal
    Signed-off-by: Avery Blanchard <avery-blanchard@proton.me>

--- container_ima.c
+++ container_ima.c
@@ -217,13 +217,15 @@ int ima_measure_image_fs(struct dentry *root, char *root_hash)
 			char *res;
 			
 			pathbuf = kmalloc(PATH_MAX, GFP_KERNEL);
-			if (!pathbuf)
+			if (!pathbuf) {
 				return -1;
+			}
 
 			res = dentry_path_raw(root, pathbuf, PATH_MAX);
                         file = filp_open(res, O_RDONLY, 0);
                         if (IS_ERR(file) || !file) {
 				pr_info("container-ima: open [%s] failed %d", res, PTR_ERR(file));
+				kfree(pathbuf);
 				return -1;
 			} else {
                                 root_hash = kprobe_measure_file(file, root_hash);
@@ -236,6 +238,7 @@ int ima_measure_image_fs(struct dentry *root, char *root_hash)
 	}
 
 	return 0;
+
 }
 /*
  * handler_post
